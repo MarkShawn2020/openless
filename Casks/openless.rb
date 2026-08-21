08d70601f993244d9be7e61e214b8973136df931
Chris233 <h-chris233@outlook.com>
2026-08-21 21:44:36 +0800

    [cask] openless 1.3.18 (manual after release) [skip ci]

--- Casks/openless.rb
+++ Casks/openless.rb
@@ -1,9 +1,9 @@
 cask "openless" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.3.17"
-  sha256 arm:   "405467056c64c05fe13771e00c316dd3b4d344333fe413b45b6bc6e828b6e742",
-         intel: "3079e07102ef3842a51f78f5d3427cfce1648db33b436e61b68183060d1f2a62"
+  version "1.3.18"
+  sha256 arm:   "746dde4d4ffa8b4464b2e52b8a73f8e18092215d871e59c6bbf766bfed53fa75",
+         intel: "36b9ba5f7926b5bb3ac7f6af3b1f5a75096b9b0f3aebe12581c91876522f2cf4"
 
   url "https://github.com/Open-Less/openless/releases/download/v#{version}-tauri/OpenLess_#{version}_#{arch}.dmg"
   name "OpenLess"
