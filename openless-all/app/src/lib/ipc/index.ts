798c495db476fcbea0c8450f47939106087539b1
markshawn2020 <shawninjuly@gmail.com>
2026-08-23 19:10:00 +0800

    feat(dictation): recover Esc-cancelled recordings

--- openless-all/app/src/lib/ipc/index.ts
+++ openless-all/app/src/lib/ipc/index.ts
@@ -86,6 +86,8 @@ export {
     startDictation,
     stopDictation,
     cancelDictation,
+    resumeCancelledRecording,
+    dismissCancelledRecordingRecovery,
     handleWindowHotkeyEvent,
 } from "./dictation"
 
